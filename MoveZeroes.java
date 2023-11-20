2fa6f6fdc6f5658a66c0b48b03c514dd51dffc6b
optiimran@gmail.com <optiimran@gmail.com>
2023-11-19 20:01:48 -0500

    Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -24,3 +24,8 @@ public class MoveZeroes {
         moveZeroes(myArr);
     }
 }
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+ */
