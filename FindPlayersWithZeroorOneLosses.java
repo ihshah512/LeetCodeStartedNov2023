c2f7951bc7c0e8c69834548b40f5ca6cfd33bc3f
optiimran@gmail.com <optiimran@gmail.com>
2023-12-03 12:09:02 -0500

    This problems helps to understand the list, hashMap, hashTree, Set concepts in detail. Although the runtime is pretty slow at this time 403 ms, which can be improved, This is working solution of problem

--- FindPlayersWithZeroorOneLosses.java
+++ FindPlayersWithZeroorOneLosses.java
@@ -1,75 +1,68 @@
 package Hashsing;
 import java.util.*;
+
 public class FindPlayersWithZeroorOneLosses {
 
-    public static void findWinners(int[][] matches) {
+    public static List<List<Integer>> findWinners(int[][] matches) {
+        Set<Integer> allWinners = new TreeSet<>();
+        Set<Integer> allLosers = new TreeSet<>();
+        Set<Integer> neverLostAny = new TreeSet<>();
+        Set<Integer> justOneLost = new TreeSet<>();
+        HashMap<Integer, Integer> loserFre = new HashMap<>();
 
-List<List<Integer>> returnList = new ArrayList<>();
-    //Declare a Set for all winners; choosing set to avoid dublicaitons
-     List<Integer> allWinners = new ArrayList<>();
-     List<Integer> allLosers = new ArrayList<>();
-     Set<Integer> neverLostAny = new TreeSet<>();
-     List<Integer> justOneLost = new ArrayList<>();
-     HashMap<Integer, Integer> loserFre = new HashMap<>();
-
-    for(int i = 0; i < matches.length; i++){
-        int[] subArr = matches[i];
-        for(int j = 0; j < subArr.length; j++){
-            int currPlayer = subArr[j];
-            if(j == 0) {
-                allWinners.add(currPlayer);
-            }else{
-                allLosers.add(currPlayer);
-                loserFre.put(currPlayer, loserFre.getOrDefault(currPlayer, 0)+1);
-            }
-            for(Map.Entry<Integer,Integer> entry : loserFre.entrySet()){
-
-                if(entry.getKey() == 1){
-                    justOneLost.add(currPlayer);
+        for (int i = 0; i < matches.length; i++) {
+            int[] subArr = matches[i];
+            for (int j = 0; j < subArr.length; j++) {
+                int currPlayer = subArr[j];
+                if (j == 0) {
+                    allWinners.add(currPlayer);
+                } else {
+                    allLosers.add(currPlayer);
+                    loserFre.put(currPlayer, loserFre.getOrDefault(currPlayer, 0) + 1);
                 }
-            }
+                //This is important pece of code thats who we count frequncy of occurance
+                if (loserFre.containsKey(currPlayer) && loserFre.get(currPlayer) == 1) {
+                    justOneLost.add(currPlayer);
+                } else if (justOneLost.contains(currPlayer)) {
+                    justOneLost.remove(Integer.valueOf(currPlayer));
+                }
 
+            }
         }
-    }
-        for(int l : allWinners){
-            if(! allLosers.contains(l)){
+
+        for (int l : allWinners) {
+            if (!allLosers.contains(l)) {
                 neverLostAny.add(l);
             }
         }
+      //  List<Integer> allWinnersList = new ArrayList<>(allWinners);
+        //List<Integer> allLosersList = new ArrayList<>(allLosers);
+        List<Integer> listNeverLost = new ArrayList<>(neverLostAny);
+        List<Integer> listJustOne = new ArrayList<>(justOneLost);
 
+       // Collections.sort(neverLostAny);
+      //  Collections.sort(justOneLost);
 
+        List<List<Integer>> result = new ArrayList<>();
+        //result.add(allWinnersList);
+       // result.add(allLosersList);
+        result.add(listNeverLost);
+        result.add(listJustOne);
 
-
-        System.out.println("All winners");
-    for(int i: allWinners){
-        System.out.print(i +" ,");
-    }
-        System.out.println();
-        System.out.println("All lossers");
-    for(int j : allLosers){
-        System.out.print(j+ " ,");
-    }
-        System.out.println();
-        System.out.println("Player never lost match");
-        for(int o : neverLostAny){
-            System.out.print(o+" ,");
-        }
-
-        System.out.println();
-        System.out.println("Player only one match lost");
-        for(int p : justOneLost){
-            System.out.print(p+" ,");
-        }
+        return result;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int[][] matches = {{1,3},{2,3},{3,6},{5,6},{5,7},{4,5},{4,8},{4,9},{10,4},{10,9}};
+        List<List<Integer>> result = findWinners(matches);
 
-    int[][] matches = {{1,3},{2,3},{3,6},{5,6},{5,7},{4,5},{4,8},{4,9},{10,4},{10,9}};
-    findWinners(matches);
-
-
+        System.out.println("All winners: " + result.get(0));
+        System.out.println("All losers: " + result.get(1));
+       // System.out.println("Players who never lost a match: " + result.get(2));
+      //  System.out.println("Players who lost exactly one match: " + result.get(3));
     }
 }
+
 /*
 you are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
 Return a list answer of size 2 where:
